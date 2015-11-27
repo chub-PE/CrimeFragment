0aa9d45d9ae8705cc94e330b65b30d513b56b57e
chub <2cs.public@gmail.com>
2015-11-27 21:23:41 +0100

    final commit of the day
    created a nested subclass for ArrayAdapter, that is using a new view I created: list_item_crime.xml. Wired em up, compiles just fine.

--- app/src/main/java/chub/birb/CrimeListFragment.java
+++ app/src/main/java/chub/birb/CrimeListFragment.java
@@ -4,8 +4,11 @@ import android.app.ListFragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,10 +18,9 @@ import java.util.ArrayList;
  */
 public class CrimeListFragment extends ListFragment
 {
-
 	private static final String TAG = "CrimeListFragment";
-
 	private ArrayList<Crime> _crimeList;
+
 	@Override
 	public void onCreate(Bundle b)
 	{
@@ -26,15 +28,53 @@ public class CrimeListFragment extends ListFragment
 		getActivity().setTitle(R.string.crimes_title);
 		_crimeList = CrimeLab.get(getActivity()).getCrimeList();
 
-		ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(
-				getActivity(), android.R.layout.simple_list_item_1, _crimeList);
+		CrimeAdapter adapter =
+				new CrimeAdapter(_crimeList);
 		setListAdapter(adapter);
 	}
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id)
 	{
-		Crime c = (Crime)(getListAdapter()).getItem(position);
+		Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 		Log.d(TAG, c.getCrimeTitle() + " was clicked");
 	}
+
+	private class CrimeAdapter extends ArrayAdapter<Crime>
+	{
+		public CrimeAdapter (ArrayList<Crime> crimes)
+		{
+			super(getActivity(), 0, crimes);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
+		{
+			//if converview null inflate a new
+			if (convertView == null)
+			{
+				convertView =
+						getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+			}
+
+			//configure the view
+			Crime c = getItem(position);
+
+			TextView titleTextView =
+					(TextView) convertView.findViewById(R.id.listCrimeTitleTextView);
+				titleTextView.setText(c.getCrimeTitle());
+
+			TextView dateTextView =
+					(TextView) convertView.findViewById(R.id.listCrimeDateTextView);
+				dateTextView.setText(c.getCrimeDate().toString());
+
+			CheckBox solvedCheckBox =
+					(CheckBox) convertView.findViewById(R.id.listCrimeSolvedCheckBox);
+			solvedCheckBox.setChecked(c.isCrimeSolved());
+
+			return convertView;
+		}
+	}
+
+
 }
