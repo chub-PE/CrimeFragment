f38fabe801378e5511f25bd37c9df986e7b8b14e
chub <2cs.public@gmail.com>
2015-11-27 15:29:27 +0100

    Created CrimeFragment class, and added base methods Created fragment_crime.xml

--- app/src/main/java/chub/birb/CrimeFragment.java
+++ app/src/main/java/chub/birb/CrimeFragment.java
@@ -1,12 +1,29 @@
 package chub.birb;
 
-import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by chub on 11/27/2015.
  */
 public class CrimeFragment extends Fragment
 {
+	private Crime _crimeObject;
+	@Override
+	public void onCreate(Bundle b)
+	{
+		super.onCreate(b);
+		_crimeObject = new Crime();
+	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle b)
+	{
+		View result = inflater.inflate(R.layout.fragment_crime, parent, false);
 
+		return result;
+	}
 }
