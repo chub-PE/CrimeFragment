cf81c054ec7489adbe637453300a6aefff70ca2f
chub <2cs.public@gmail.com>
2015-11-27 14:49:39 +0100

    Created CrimeFragment class, and added base methods Added a line of code to MainActivity

--- app/src/main/java/chub/birb/MainActivity.java
+++ app/src/main/java/chub/birb/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,9 +15,12 @@ public class MainActivity extends FragmentActivity
 {
 
     @Override
+    /**Called when the activity is first created*/
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
 
     }
 
