028e21ed82332d45fe895dba55670beb3c2cef8c
chub <2cs.public@gmail.com>
2015-11-27 15:35:03 +0100

    Completed base funcionality of CrimeFragment Hooked up CrimeFragment in MainActivity

--- app/src/main/java/chub/birb/CrimeFragment.java
+++ app/src/main/java/chub/birb/CrimeFragment.java
@@ -2,16 +2,22 @@ package chub.birb;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by chub on 11/27/2015.
+ * FUCK YOU LINT I WRITE HEADERS WHENEVER I PLEASE
  */
 public class CrimeFragment extends Fragment
 {
 	private Crime _crimeObject;
+	private EditText _titleField;
+
 	@Override
 	public void onCreate(Bundle b)
 	{
@@ -23,6 +29,23 @@ public class CrimeFragment extends Fragment
 	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle b)
 	{
 		View result = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+		_titleField = (EditText)result.findViewById(R.id.crime_title);
+		_titleField.addTextChangedListener(new TextWatcher()
+		{
+			public void onTextChanged(CharSequence c, int start, int before, int count)
+			{
+				_crimeObject.setCrimeTitle(c.toString());
+			}
+			public void beforeTextChanged(CharSequence c, int start, int count, int after)
+			{
+				//blank
+			}
+			public void afterTextChanged(Editable c)
+			{
+				//blanks
+			}
+		});
 
 		return result;
 	}
--- app/src/main/java/chub/birb/MainActivity.java
+++ app/src/main/java/chub/birb/MainActivity.java
@@ -3,6 +3,7 @@ package chub.birb;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -21,7 +22,12 @@ public class MainActivity extends FragmentActivity
         setContentView(R.layout.activity_main);
 
         FragmentManager fm = getSupportFragmentManager();
-
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		if (fragment == null)
+		{
+			fragment = new CrimeFragment();
+			fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+		}
     }
 
 
